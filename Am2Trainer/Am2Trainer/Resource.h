be6d5a9f125562160ba8f99fa7ee7e730585d655
maxperuchi <maxwelmello@yahoo.com.br>
2014-03-23 19:29:49 -0300

    Army men 2 trainer
    Still need to fix the base address on the startup

---
+++ Am2Trainer/Am2Trainer/Resource.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Am2Trainer.rc
+//
+
+#define IDS_APP_TITLE			103
+
+#define IDC_BTN_FECHAR			122
+#define IDC_BTN_RECUPERAR		123
+#define IDR_MAINFRAME			128
+#define IDD_AM2TRAINER_DIALOG	102
+#define IDI_AM2TRAINER			107
+#define IDI_SMALL				108
+#define IDC_AM2TRAINER			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
